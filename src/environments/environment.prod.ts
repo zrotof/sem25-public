9effbc6ae5806bd564d5c0ba34930abe780fdf05
zrotof <manduel21@gmail.com>
2025-02-05 06:05:34 +0100

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   streamingLink: "https://stream.az-multimedia.com:3584/hybrid/play.m3u8",
-  apiBaseUrl : "https://www.api-test.amadou-ba.sn/v1/"
+  apiBaseUrl : "https://www.campagne-api.purs.cm/v1/"
 };
