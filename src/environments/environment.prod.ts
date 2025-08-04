e68fa48cf22bf19f0b4b5e05c64abc42005c7a98
Samuel Mandeng <zrotof@Air-de-Samuel.lan>
2025-08-04 05:41:14 +0200

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   streamingLink: "https://stream.az-multimedia.com:3584/hybrid/play.m3u8",
-  //apiBaseUrl : "https://api-test.matomba2025.com/v1/",
-  apiBaseUrl: "http://api-demo.matomba2025.com/v1/"
+  apiBaseUrl: "https://api-demo.matomba2025.com/v1/"
 };
